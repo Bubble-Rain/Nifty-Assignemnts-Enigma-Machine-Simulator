86396ac4983a886c1242d80210055bbb1f1626af
Jericho Limpin <jerald.limpin@gmail.com>
2023-05-11 11:07:52 +1000

    Completed Milestone 2

--- EnigmaModel.py
+++ EnigmaModel.py
@@ -11,6 +11,7 @@ class EnigmaModel:
         """Creates a new EnigmaModel with no views."""
         self._views = [ ]
         self._key_pressed_dict = dict.fromkeys(string.ascii_uppercase, 0)
+        self._lamp_on_dict = dict.fromkeys(string.ascii_uppercase, 0)
 
     def add_view(self, view):
         """Adds a view to this model."""
@@ -25,14 +26,16 @@ class EnigmaModel:
         return self._key_pressed_dict[letter]
 
     def is_lamp_on(self, letter):
-        return False        # In the stub version, lamps are always off
+        return self._lamp_on_dict[letter]
 
     def key_pressed(self, letter):
         self._key_pressed_dict[letter] = True
+        self._lamp_on_dict[letter] = True
         self.update()
 
     def key_released(self, letter):
         self._key_pressed_dict[letter] = False
+        self._lamp_on_dict[letter] = False
         self.update()
 
     def get_rotor_letter(self, index):
